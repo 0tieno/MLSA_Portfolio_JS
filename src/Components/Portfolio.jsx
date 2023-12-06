aa47d8b23915942a493529fbc3a2246e7bca2ee7
Sir Ronney Otieno <118272387+0tieno@users.noreply.github.com>
2023-12-06 06:25:21 +0000

    made changes on the site look and details

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/work-setup.jpg";
 
 const imageAltText = "desktop with books and laptop";
 
@@ -37,19 +37,19 @@ const projectList = [
     url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Web Development for Beginners - A Curriculum",
     description:
       "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    url: "0tieno.github.io/Food-Website/",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "https://0tieno.github.io/My_Portfolio_website/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Creative Agency Website",
     description:
       "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
     url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
